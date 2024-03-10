aa2987968615ef69a0d062b72b6dfe0555f49e95
Tawanda23 <tawanda.mafukidze@outlook.com>
2024-03-10 22:08:38 +0000

    updates html

--- my-site/static/assets/js/index.js
+++ my-site/static/assets/js/index.js
@@ -1,9 +1,8 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch(
-        "https://lambda.ajharresume.com/"
-    );
+        "https://3bfjjelldc5c267cshehqz5rvq0bcssc.lambda-url.eu-west-2.on.aws/");
     let data = await response.json();
-    counter.innerHTML = `You are visitor number: ${data}`;
+    counter.innerHTML = `Views: ${data}`;
 }
 updateCounter();
